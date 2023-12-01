c0047a8a5e991cb332d825ccfec1dd40677d73fa
Denner Lopes <92930966+DS3ttx@users.noreply.github.com>
2023-12-01 07:33:13 -0300

    Retirar Funções do CSV

--- src/ControleDeLocacao/ControleDeLocacao.cpp
+++ src/ControleDeLocacao/ControleDeLocacao.cpp
@@ -8,68 +8,6 @@
 
 using namespace std;
 
-//PRIVATE
-time_t encontrar_data_alocacao(long cpf, int codigo) {
-    ifstream banco_de_locacoes("../../data/banco_de_locacoes.csv");
-
-    if (!banco_de_locacoes.is_open()) {
-        throw ErroArquivo ("Erro ao abrir o arquivo");
-    }
-
-    string linha;
-    getline(banco_de_locacoes, linha);
-    stringstream linhaStream(linha);
-
-    string campo;
-    vector<string> celulas;
-
-    while (getline(linhaStream, campo, ',')) {
-        celulas.push_back(campo);
-    }
-
-    long cpf_csv = stol(celulas[0]);
-    if (cpf_csv == cpf) {
-        string datetime = celulas[3];
-        tm tmStruct = {};
-        istringstream ss(datetime);
-        ss >> get_time(&tmStruct, "%Y-%m-%d %H:%M:%S");
-        time_t tempo = mktime(&tmStruct);
-        cout << "Valor de time_t: " << tempo << endl;
-        banco_de_locacoes.close();
-        return tempo;
-    }
-
-    banco_de_locacoes.close();
-    // throw exception
-}
-
-void gravar_locacoes_csv(long int cpf_cliente, map<int, int> locacoes){
-    ofstream banco_de_locacoes("../../data/banco_de_locacoes.csv", ios::app);
-    if (!banco_de_locacoes.is_open()) {
-        throw ErroArquivo ("Erro ao abrir o arquivo");
-    }
-
-    time_t agora = time(0);
-
-    for (auto& info_midia : locacoes) {
-        banco_de_locacoes << cpf_cliente << "," << info_midia.first << "," << info_midia.second << "," << ctime(&agora) << ",,\n";
-    }
-
-    banco_de_locacoes.close();
-}
-
-int calcula_dias(time_t data_locacao){
-    time_t data_devolucao = time(0);
-    const int segundosPorDia = 60 * 60 * 24;
-
-    int diferencaSegundos = static_cast<int>(difftime(data_locacao, data_devolucao));
-    int diferencaDias = diferencaSegundos / segundosPorDia;
-
-    return diferencaDias;
-}
-
-//PUBLIC
-
 void Locador::alugar_midias(long int cpf_cliente, map<int, int> locacoes, ControleCliente clientes){ // futuramente adicionar um handler de exceção para quando o cliente quer alugar uma mídia que não existe no sistema.
     clientes.escrever_locacoes_cliente(cpf_cliente, locacoes);
     cout << "######### Recibo de Locação ###########" << endl;
