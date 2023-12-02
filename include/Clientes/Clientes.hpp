0b788aac3a235bb54e70c3085d3ae056df699fad
jpmouraf <joaopfurtado@outlook.com>
2023-12-02 03:31:26 -0300

    Comentando Clientes

--- include/Clientes/Clientes.hpp
+++ include/Clientes/Clientes.hpp
@@ -8,59 +8,126 @@
 
 using namespace std;
 
-struct InformacoesLocacao{
-    map<int, int> locacoes;
-    int dias;
+/**
+ * Estrutura para armazenar informações sobre locações de um cliente.
+ */
+struct InformacoesLocacao {
+    map<int, int> locacoes; ///< Mapa que associa códigos de locação a dias.
+    int dias; ///< Número de dias de locação.
 };
 
+/**
+ * Classe que representa um cliente.
+ */
 class Cliente {
-    private:
-        string _nome;
-        long int _cpf;
- 
-    public:
-        Cliente(string nome, long int cpf);
+private:
+    string _nome; ///< Nome do cliente.
+    long int _cpf; ///< CPF do cliente.
 
-        long int getCPF();
-        string getNome();
+public:
+    /**
+     * Construtor da classe Cliente.
+     */
+    Cliente(string nome, long int cpf);
+
+    /**
+     * retorna o CPF do cliente.
+     */
+    long int getCPF();
+
+    /**
+     * retorna o nome do cliente.
+     */
+    string getNome();
 };
 
+/**
+ * Classe para controle de clientes.
+ */
 class ControleCliente {
-    private:
-        list<Cliente> _clientes;
+private:
+    list<Cliente> _clientes; ///< Lista de clientes.
 
-        void escrever_data_devolucao();
-        time_t encontrar_data_alocacao(long cpf, int codigo);
-        int calcula_dias(time_t data_locacao);
-        map<int, int> locacoes_cliente(long cpf);
+    void escrever_data_devolucao();
 
-    public:
-        bool validarCPF(long int cpf);
-        void cadastrar_cliente(long int cpf, string nome);
-        void remover_cliente(long int cpf);
-        void listar_codigo();
-        void listar_nome();
-        void escrever_locacoes_cliente(long int cpf_cliente, map<int, int> locacoes);
-        InformacoesLocacao informacoes_locacao(long cpf);
+    /**
+     * Encontra a data da alocação feita pelo cliente baseado em seu cpf e pelo código do filme
+     */
+    time_t encontrar_data_alocacao(long cpf, int codigo);
+
+    /**
+     * Calcula o número de dias desde a data de locação.
+     */
+    int calcula_dias(time_t data_locacao);
+
+    /**
+     * Recebe o cpf e o codigo e armazena as locações
+     */
+    map<int, int> locacoes_cliente(long cpf);
+
+public:
+    /**
+     * Valida o cpf de acordo com a sua entrada. Verdadeiro se o CPF for válido, falso caso contrário.
+     */
+    bool validarCPF(long int cpf);
+
+    /**
+     * Cadastra um novo cliente a partir do seu cpf e nome
+     */
+    void cadastrar_cliente(long int cpf, string nome);
+
+    /**
+     * Remove um cliente cadastrado de acordo com o seu cpf
+     */
+    void remover_cliente(long int cpf);
+
+    /**
+     * Lista os clientes em ordem de cpf
+     */
+    void listar_codigo();
+
+    /**
+     * Lista os clientes em ordem alfabetica
+     */
+    void listar_nome();
+
+    /**
+     * Escreve as locações de um cliente e a armazena no map criado
+     */
+    void escrever_locacoes_cliente(long int cpf_cliente, map<int, int> locacoes);
+
+    /**
+     * Obtém informações de locação para um cliente.
+     */
+    InformacoesLocacao informacoes_locacao(long cpf);
 };
 
+/**
+ * Classe para exceção relacionada a clientes.
+ */
 class ExcecaoCliente : public std::exception {
 private:
-    const char* _mensagem;
-    
+    const char* _mensagem; ///< Mensagem de exceção.
+
 public:
+
     ExcecaoCliente(const char* mensagem) : _mensagem(mensagem) {}
-    
-    virtual const  char* what() const noexcept {
+
+    /**
+     * retorna a mensagem de excecao de acordo com o tipo especifico necessario
+     */
+    virtual const char* what() const noexcept {
         return _mensagem;
     }
 };
 
+/**
+ * Classe para exceção relacionada a erro ao abrir arquivo.
+ */
 class ErroAbrirArquivo : public runtime_error {
-    public:
-         ErroAbrirArquivo() : std::runtime_error("Erro ao abrir o arquivo.") {}
+public:
+
+    ErroAbrirArquivo() : std::runtime_error("Erro ao abrir o arquivo.") {}
 };
-
-
 
 #endif
