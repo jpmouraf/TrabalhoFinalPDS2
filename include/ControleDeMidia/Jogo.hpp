bfc0115d049b95854f2b10efbce407329297a599
jpmouraf <149010897+jpmouraf@users.noreply.github.com>
2023-12-02 16:08:18 -0300

    Update Jogo.hpp

--- include/ControleDeMidia/Jogo.hpp
+++ include/ControleDeMidia/Jogo.hpp
@@ -7,13 +7,34 @@
 
 using namespace std;
 
+/**
+ * Classe Jogo, subclasse de Midia.
+ * É um tipo de mídia extra da nossa locadora
+*/
 class Jogo : public Midia {
     private:
+/**
+ * Defini o tipo de midia como "jogo"
+*/
         static const string _tipo;
     public:
+/**
+ * Construtor da classe Jogo
+*/
         Jogo(int codigo_numerico, string titulo, int unidades_disponiveis);
+/**
+ * Calcula a locação do tipo Jogo.
+ * É aplicada uma multa de 5 reais caso o cliente retorno o jogo arranhado.
+ * Essa variável é calculada aleatoriamente
+*/
         int calcular_locacao(int dias) override;
+/**
+ * Imprime as informações do jogo.
+*/
         void imprimir_info() override;
+/**
+ * Retorna o tipo "Jogo"
+*/
         string gettipo() override;
 };
 
