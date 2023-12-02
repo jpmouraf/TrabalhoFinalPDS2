af63ec5da463ce9e6cd02ec8e71f9f216df6f8b1
jpmouraf <149010897+jpmouraf@users.noreply.github.com>
2023-12-02 16:08:45 -0300

    Update Midia.hpp

--- include/ControleDeMidia/Midia.hpp
+++ include/ControleDeMidia/Midia.hpp
@@ -6,6 +6,10 @@
 
 using namespace std;
 
+/**
+ * É a superclasse de todas as mídias.
+ * Declara as variaveis comuns: Código numérico, Título e Unidades disponíveis
+*/
 class Midia {
     private:
         int _codigo_numerico;
@@ -13,11 +17,23 @@ class Midia {
         int _unidades_disponiveis;
         
     public:
+/**
+* Construtor da classe Midia
+*/
         Midia(int codigo_numerico, string titulo, int unidades_disponiveis);
+/**
+* Retorna as variaveis que foram declaradas como privadas
+*/
         int getCodigoNumerico();
         string getTitulo();
         int getUnidadesDisponiveis();
+/**
+ * Imprime as informações: código, titulo e unidades disponiveis
+*/
         virtual void imprimir_info();
+/**
+ * Declara os metodos virtuais puros que serão sobrescritos nas classes filhas
+*/
         virtual string gettipo() = 0;
         virtual int calcular_locacao(int dias) = 0;
 };
